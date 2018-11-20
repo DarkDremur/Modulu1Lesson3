2d7cb790aaa6a54e955e1edce57d95e317e4f161
Максим Пилипенко <@local.itstep>
2018-11-20 21:46:10 +0600

    Добавьте файлы проекта.

---
+++ Project4/Source.cpp
@@ -0,0 +1,210 @@
+//11
+/*#include <iostream>
+#include <stdio.h>
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a, b, x;
+
+	printf("¬ведите число первой переменной:");
+	scanf_s("%d", &a);
+	printf("¬ведите число первой переменной:");
+	scanf_s("%d", &b);
+
+	x = a;
+	a = b;
+	b = x;
+
+	printf("b = %d\n", x);
+	printf("a = %d\n", a);
+
+	system("pause");
+}*/
+//10
+#include <iostream>
+#include <stdio.h>
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a, b, x;
+
+	printf("¬ведите число первой переменной:");
+	scanf_s("%d", &a);
+	printf("¬ведите число первой переменной:");
+	scanf_s("%d", &b);
+
+	x = a;
+	a = b;
+	b = x;
+
+	printf("b = %d\n", b);
+	printf("a = %d\n", a);
+
+
+	system("pause");
+}
+//9
+/*#include <iostream>
+#include <stdio.h>
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n, s, p;
+
+	printf("¬видите площадь:");
+	scanf_s("%d", &s);
+	printf("¬ведите кол.жителей:");
+	scanf_s("%d", &n);
+
+	p = n / s;
+
+	printf("ѕлотность население равна: %d\n", p);
+	system("pause");
+	
+}
+//8
+/*#include <iostream>
+#include <stdio.h>
+void main()
+{
+	setlocale(LC_ALL, "");
+	int m, v, r;
+
+	printf("¬ведите массу:");
+	scanf_s("%d", &m);
+	printf("¬ведите объЄм:");
+	scanf_s("%d", &v);
+
+	r = m / v;
+
+	printf("плотность равна: %d\n", r);
+
+		system("pause");
+}
+//7 b
+/*#include <iostream>
+#include <stdio.h>
+void main()
+{
+
+	setlocale(LC_ALL, "");
+	int x, a, b;
+
+	printf("¬ведите число а:");
+	scanf_s("%d", &a);
+	printf("¬ведите число b:");
+	scanf_s("%d", &b);
+
+	x = 3.56 * a + b * b * b - 5.8*b*b + 3.8 * a - 1.5;
+	printf("x pавен: %d\n", x);
+
+	system("pause");
+
+}
+//7a
+/*#include <iostream> 
+#include <stdio.h> 
+void main()
+{
+
+	setlocale(LC_ALL, "");
+
+	int z, x, y;
+	printf("¬ведите числo x:");
+	scanf_s("%d", &x);
+	printf("¬ведите числo y:");
+	scanf_s("%d", &y);
+
+	z = x * x * x - 2.5 * x * y + 1.78 * x * x - 2, 5 * y + 1;
+
+	printf("z равeн: %d\n", z);
+
+	system("pause");
+}
+//4
+/*#include <iostream> 
+#include <stdio.h> 
+void main()
+{
+
+	setlocale(LC_ALL, "");
+
+	int d, r;
+	printf("¬ведите числo радиуса:");
+	scanf_s("%d", &r);
+
+	d = r * 2;
+
+	printf("d равeн: %d\n", d);
+
+	system("pause");
+}
+//3
+/*#include <iostream> 
+#include <stdio.h> 
+void main()
+{
+
+	setlocale(LC_ALL, "");
+
+	int a, p, b;
+	printf("¬ведите сумму стороны квадрата:");
+	scanf_s("%d", &a);
+	
+	b = 4;
+	p = b * a;
+
+	printf("p равeн: %d\n", p);
+
+	system("pause");
+}
+
+//2 b
+/*#include <iostream> 
+#include <stdio.h> 
+void main()
+{
+
+	setlocale(LC_ALL, "");
+
+	int a, x;
+	printf("¬ведите число A:");
+	scanf_s("%d", &a);
+
+	x = 12 * a * a + 7 * a - 16;
+
+	printf("x равeн: %d\n", x);
+
+	system("pause");
+}
+//2 a
+/*#include <iostream> 
+#include <stdio.h> 
+void main()
+{
+
+	setlocale(LC_ALL, "");
+
+	int y, x;
+	printf("¬ведите число X:");
+	scanf_s("%d", &x);
+
+	y = 7 * x * x - 3 * x + 6;
+
+	printf("y равeн: %d\n", y);
+
+	system("pause");
+}
+
+//1
+/*#include <iostream> 
+#include <stdio.h> 
+void main()
+{
+
+	setlocale(LC_ALL, "");
+	printf("a) 510\t\t б)100t\t\t в)x25\n");
+	printf("  7см\t\t   1949v\t\t xy\n");
+
+	system("pause");
+}*/
